8bbe90b6c92405162a0392d7f822e5c1b0e67188
Yurii Zinko <yurazinko@outlook.com>
2017-01-26 14:15:24 +0200

    added breadcrumbs

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -7,4 +7,15 @@ module GroupsHelper
 		end
 		index
 	end
+
+	def build_breadcrumbs(group)
+		result = []
+    group.ancestors.each do |ancestor|
+    	result << content_tag(:li, class: "breadcrumb-item") do 
+    		link_to(ancestor.name, ancestor)
+    	end
+    end
+    result << content_tag(:li, group.name, class: "breadcrumb-item-active")
+    result.join('').html_safe
+	end
 end
